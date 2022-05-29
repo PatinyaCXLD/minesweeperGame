8610674ba7b09d4f783f1e7b8ef14199d1929b43
Patinya Saichantadee <patinya.saic@ku.th>
2022-05-29 21:49:44 +0700

    This is updated progress, most of program are functioned but there're still some bugs.

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ def randomMines(mode):
         row = 10
         col = 8
     mines = []
-    weight = [mine/(row*col),100-(mine/(row*col))]
+    bombs = mine
+    rate = mine/(row*col)
+    weight = [rate,100-rate]
     while mine > 0: 
         for i in range(col):
             if mine == 0 : break
@@ -24,7 +26,7 @@ def randomMines(mode):
                         mine-=1
                         mines.append((i,j))
                 else : break
-    return mines
+    return [mines,bombs]
 
 def createTable(mode):
     if mode == "hard" :
@@ -45,7 +47,7 @@ def createTable(mode):
         line.clear()
     
     return table
-def isVaild(direction,current):
+def isVaild(direction,current,flag):
     if direction == "N":
         check = (current[0]-1,current[1])
     if direction == "W":
@@ -62,41 +64,97 @@ def isVaild(direction,current):
         check = (current[0]+1,current[1]+1)
     if direction == "SW":
         check = (current[0]+1,current[1]-1)
-    if (check[0]<0 or check[0] >= len(table)) or (check[1]<0 or check[1] >= len(table[0])) :
-        return False
+    if flag :
+        if (check[0]<0 or check[0] >= len(table)) or (check[1]<0 or check[1] >= len(table[0])) :
+            return False
     elif (check in mines):
         return False
     return check
-def addNumber(dic,mines):
-    for j in mines:
+def addNumber(dic):
+    for j in dic:
         for i in ["N","W","S","E","NE","NW","SE","SW"]:
-            check = isVaild(i,j)
-            if check :
-                dic[(check[0],check[1])] += 1
+            check = isVaild(i,j,0)
+            if not check :
+                dic[j] += 1
+def goCheck(target,dic):
+    mustcheck = []
+    mustcheck.insert(0,target)
+    show = []
+    while mustcheck :
+        current = mustcheck.pop()
+        if dic[current] == 0:
+            for i in ["N","W","S","E","NE","NW","SE","SW"] :
+                check = isVaild(i,current,1)
+                if check :
+                    if check not in show:
+                        show.append(check)
+                        if dic[check] == 0:
+                            mustcheck.insert(0,check)
+        else:
+            show.append(current)
+                
+    return show
 
+def adjustTable(show,table,number):
+    for i in show:
+        if number[i] == 0 : table[i[0]][i[1]] = "⬜"
+        if number[i] == 1 : table[i[0]][i[1]] = "1️⃣ "
+        if number[i] == 2 : table[i[0]][i[1]] = "2️⃣ "
+        if number[i] == 3 : table[i[0]][i[1]] = "3️⃣ "
+        if number[i] == 4 : table[i[0]][i[1]] = "4️⃣ "
+        if number[i] == 5 : table[i[0]][i[1]] = "5️⃣ "
+        if number[i] == 6 : table[i[0]][i[1]] = "6️⃣ "
+        if number[i] == 7 : table[i[0]][i[1]] = "7️⃣ "
+        if number[i] == 8 : table[i[0]][i[1]] = "8️⃣ "
+    return table
+        
 import random as rd
-
-mines = randomMines("easy")
-table = createTable("easy")
+from time import sleep
+mines,bombs = randomMines("medium")
+table = createTable("medium")
 
 print(mines)
 for i in table:
     print("".join(i))
-    
+print()
 death = 0
 win = 0
 number = {}
+showed = []
 for i in range(len(table)):
     for j in range(len(table[i])):
         number[(i,j)] = 0
+for i in mines:
+    number.pop(i)
+addNumber(number)
+allSafe = list(number.keys())
+k=0
 while not (death or win) :
-    target = input()
-    target.strip("(")
-    target.strip(")")
+    bombleft = 0
+    while table[allSafe[k][0]][allSafe[k][1]] != "🟩":
+        k+=1
+    target = str(allSafe[k])
+    # target = input()
+    target = target.strip('(')
+    target = target.strip(')')
     split = target.index(",")
-    i = int(target[0:split])
+    i = int(target[:split])
     j = int(target[split+1:])
-    addNumber(number,mines)
-    death = 1
-print(i,j)
-print(number)
+    target = (i,j)
+    if (i,j) in mines : 
+        death = 1
+        print("You're dead, try again later.")
+    else:
+      show = goCheck(target,number)
+    showed = list(set(show+showed))
+    table = adjustTable(show,table,number)
+    for i in table:
+        print("".join(i))
+        bombleft += i.count("🟩")
+        
+    if bombleft == bombs:
+        print("You defused all bomb! Congratulation.")
+        win = 1
+    k+=1
+    print()
+    sleep(0.2)
